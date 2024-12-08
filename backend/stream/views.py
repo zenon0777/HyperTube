224e64a0b9ad1fd3db57b5b8b100bbfa0e656ffb
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-12-08 21:14:48 +0100

    add loading torrent from qeurey

--- backend/stream/views.py
+++ backend/stream/views.py
@@ -4,14 +4,10 @@ from .services.stream import TorrentStream
 
 
 def stream_torrent(request):
-    # torrent_url = request.GET.get('torrent_url', None)
-    # if not torrent_url:
-    #     return JsonResponse({'status': 'error', 'message': 'Torrent URL is required!'}, status=400)
-    torrent_url = "https://yts.mx/torrent/download/C7F59A9DA3B3615CA92E8587F1EED9903F098163"
+    torrent_url = request.GET.get('torrent_url', None)
+    if not torrent_url:
+        return JsonResponse({'status': 'error', 'message': 'Torrent URL is required!'}, status=400)
     ts = TorrentStream()
-    # s.download_torrent_file()
-    # s.download_torrent()
-    # return s.stream_torrent()
     ts.init_torrent_file(torrent_url)
     ts.add_torrent()
     vrange = request.headers.get('Range', None)
