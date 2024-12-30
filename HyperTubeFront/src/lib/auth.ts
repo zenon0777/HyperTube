274c52d599ed4b555eb9570bdfb3185a0b71a261
zenon0777 <abderrahmane.daifi@protonmail.com>
2024-12-30 20:30:10 +0100

    feat(auth): add login and register with frontend/backend integration

---
+++ HyperTubeFront/src/lib/auth.ts
@@ -0,0 +1,32 @@
+import api from "./axios";
+
+export const authService = {
+  async register(userData: any) {
+    const response = await api.post("/auth/register/", userData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  },
+
+  async login(credentials: any) {
+    const response = await api.post("/auth/login/", credentials);
+    return response.data;
+  },
+
+  async forgotPassword(email: string) {
+    const response = await api.post("/password/reset/", { email });
+    return response.data;
+  },
+
+  async resetPassword(data: any) {
+    const response = await api.post("/password/reset/confirm/", data);
+    return response.data;
+  },
+
+  async updateProfile(userId: string, data: any) {
+    const response = await api.patch(`/users/${userId}/`, data);
+    return response.data;
+  },
+};
---
+++ HyperTubeFront/src/lib/axios.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  if (config.data instanceof FormData) {
+    config.headers['Content-Type'] = 'multipart/form-data';
+  } else {
+    config.headers['Content-Type'] = 'application/json';
+  }
+  return config;
+});
+
+export default api;
